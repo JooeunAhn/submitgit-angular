0d3811128ce78292b0ff8e3b54be27bd230fada0
JooeunAhn <jflow@hanyang.ac.kr>
2017-05-22 01:52:00 +0900

    reset..

--- src/app/pages/profile/profile-add/profile-add.component.ts
+++ src/app/pages/profile/profile-add/profile-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, AbstractControl, FormBuilder, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../../../shared/services/auth.service";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-profile-add',
@@ -7,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileAddComponent implements OnInit {
 
-  constructor() { }
+  public form:FormGroup;
+  public is_prof:AbstractControl;
+  public name:AbstractControl;
+  public sid:AbstractControl;
+  public github_token:AbstractControl;
+
+
+  constructor(fb:FormBuilder, private router: Router, private authService: AuthService, private http: Http) {
+    this.form = fb.group({
+      'is_prof': [false, Validators.compose([Validators.required])],
+      'name': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      'sid': [''],
+      'github_token': [''],
+    });
+    this.is_prof = this.form.controls['is_prof'];
+    this.name = this.form.controls['name'];
+    this.sid = this.form.controls['sid'];
+    this.github_token = this.form.controls['github_token']
+  }
 
   ngOnInit() {
     console.log('profile-add');
+  }
+
+  onSubmit(values){
+    console.log(values);
   }
 
 }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,11 +4,16 @@ import { ProfileAddComponent } from './profile-add/profile-add.component';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 import { ProfileComponent } from './profile.component';
 import {routing} from "./profile.routing";
+import {ReactiveFormsModule, FormsModule} from "@angular/forms";
+import {NgaModule} from "../../theme/nga.module";
 
 @NgModule({
   imports: [
     CommonModule,
     routing,
+    ReactiveFormsModule,
+    FormsModule,
+    NgaModule,
   ],
   declarations: [ProfileAddComponent, ProfileViewComponent, ProfileComponent]
 })
